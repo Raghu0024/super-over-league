ba0f1227e94e89162f3058d4033714cb4b0a48f7
Raghu <vamsiraghu0024@gmail.com>
2023-03-20 11:56:11 +0000

    commiting the first code

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <div>
+      <h1>Super Over League</h1>
+    </div>
+    <img
+      className="img1"
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+    />
+    <img
+      className="img2"
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
